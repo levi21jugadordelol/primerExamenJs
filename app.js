f7d8a1ffd860c8bc814469442ff012ab1dd22f88
saul rodriguez <editor.taisei@gmail.com>
2025-04-11 18:52:34 -0500

    trabajo terminado

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import { cambiandoColor } from "./dom/colorTitulo.js";
 import { carruseImagenes } from "./dom/carruselImagenes.js";
 import { relojDIgital } from "./dom/relojDigital.js";
 import { leyendaImagen } from "./dom/leyendaImagenes.js";
+import { rotacionIcono } from "./dom/icono.js";
 
 const d = document;
 
@@ -10,4 +11,5 @@ d.addEventListener("DOMContentLoaded", (e) => {
   carruseImagenes(".imagen", ".container_carrusel");
   relojDIgital("hora", ".cabezera_hora", "fecha", ".cabezera_fecha");
   leyendaImagen(".container_carrusel", ".imagen");
+  rotacionIcono(".iconoRed");
 });
--- dom/icono.js
+++ dom/icono.js
@@ -1,1 +1,17 @@
-export function rotacionIcono() {}
+export function rotacionIcono(clase_icono) {
+  const d = document;
+
+  d.addEventListener("mouseover", (e) => {
+    if (e.target.matches && e.target.matches(clase_icono)) {
+      e.target.style.color = "goldenrod";
+      e.target.style.transform = "rotate(180deg)";
+    }
+  });
+
+  d.addEventListener("mouseout", (e) => {
+    if (e.target.matches && e.target.matches(clase_icono)) {
+      e.target.style.color = "";
+      e.target.style.transform = "rotate(0deg)";
+    }
+  });
+}
